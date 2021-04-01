f53393756e883e04d40a7924ec1205ea5c7e6da9
Larisa Pylaeva <laralord@yandex.ru>
2021-04-01 10:12:09 +0300

    All added

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int sum = 500_000_000;
+        int total = balance + sum;
+        System.out.println(total);
+
+    }
+}
